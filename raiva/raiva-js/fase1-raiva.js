4463af7ebd384243935717d9c5275f6142d40088
PalomaBarreto <palomaufrrj@gmail.com>
2021-08-18 02:27:29 -0300

    Ajustes no css

--- raiva/raiva-js/fase1-raiva.js
+++ raiva/raiva-js/fase1-raiva.js
@@ -2,43 +2,43 @@ function fase2() {
     
     document.write(`
     <div class="container">
-
+    
     <h1 class="pergunta">Riley e seus pais acabaram de chegar de mudança para San Francisco e ela não está gostando
-        de nada. Está muito chateada. O que o trio de emoções decide fazer para tentar animá-la?
+    de nada. Está muito chateada. O que o trio de emoções decide fazer para tentar animá-la?
     </h1>
-
+    
     <div class="opcoes">
-        <p>
-        1- Deixam a Raiva comandar, então Riley fica pistola e se isola no quarto.
-        </p>
-
-        <p>
-        2- Alegria decide colocar lembranças felizes com a família para que Riley enxergue um lado positivo e vá brincar na sala com seus pais.
-        </p>
+    <p>
+    1- Deixam a Raiva comandar, então Riley fica pistola e se isola no quarto.
+    </p>
+    
+    <p>
+    2- Alegria decide colocar lembranças felizes com a família para que Riley enxergue um lado positivo e vá brincar na sala com seus pais.
+    </p>
     </div>
-
+    
     <a href="../raiva/fase2-raiva.html">
-        Escolher
+    Escolher
     </a>
-
-
+    
+    
     </div>
     `)
 }
 
 function escolhaRaiva() {
-      
+    
     
     var opcoes = "Digite 1 ou 2, conforme sua escolha\n 1 = Alegria coloca a tristeza no cantinho da leitura. A raiva acredita ser um absurdo abandonar Woody e Buzz Lightyear.\n 2 = Tristeza se joga no chão e chora pois não quer doar nada."
-
+    
     var escolha
-
+    
     while (escolha != 1 && escolha != 2) {
-
-         
+        
+        
         escolha = prompt(opcoes)
         
-
+        
         if (escolha == 1) {
             fase2()
             break
@@ -62,4 +62,4 @@ escolhaRaiva()
 
 
 
-    
+
